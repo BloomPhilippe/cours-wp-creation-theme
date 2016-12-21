77d3f98c0fdd8682d9566e3d84e06f63c8728dc8
BloomPhilippe <bloom_phil@hotmail.com>
2016-12-21 20:00:55 +0100

    tweets

--- extensions/Tweets.php
+++ extensions/Tweets.php
@@ -10,4 +10,10 @@ $access_token_secret = "AzNJhPjiLROFsTo3JdrFhuT6XJ30v8XNYsvkFNExBoFB8";
 $connection = new TwitterOAuth("AJILqysIVF4zkLsc1jAw5BmNP", "17wr6pS2uskTxyryRYrjfm2vZBSrL6oh32qzONrPGUcE57kpgN", $access_token, $access_token_secret);
 $content = $connection->get("account/verify_credentials");
 
-var_dump($content);
+$result = $connection->get('statuses/user_timeline', [
+    'screen_name' => 'getbootstrap',
+    'exclude_replies' => true,
+    'count' => 50
+]);
+
+$tweets = array_splice($result, 0, 50);
